b523b267c16b2083d3c83564cdb7bb408773f97d
DaedalusMDW <26656082+DaedalusMDW@users.noreply.github.com>
2017-10-20 05:01:50 -0500

    added GPL block

--- PY/Input.py
+++ PY/Input.py
@@ -1,3 +1,24 @@
+####
+# bge_keymapper_class: a class based keymapper with joystick support
+# Copyright (C) 2017  DaedalusMDW
+#
+# This file is part of bge_keymapper_class.
+#
+#    bge_keymapper_class is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    bge_keymapper_class is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with bge_keymapper_class.  If not, see <http://www.gnu.org/licenses/>.
+#
+####
+
 ## INPUT PROCESSING ##
 
 
